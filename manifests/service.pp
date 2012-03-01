758e10d187db01b12d58752175b4c265a6e9c27e
Jan Vansteenkiste <jan@vstone.eu>
2012-03-01 16:08:07 +0000

    Fix variable name for service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,5 @@
 class mcollective::service {
-  case $operatingsystem {
+  case $::operatingsystem {
     ubuntu,debian,redhat,centos: { include mcollective::service::actual }
     default: { notice("${hostname}: mcollective: module does not yet support $operatingsystem") }
   }
