8d6442721c17cd8661c547ee40e2d6d270a10d10
Jan Vansteenkiste <jan@vstone.eu>
2012-03-05 13:04:45 +0000

    Fix quoting in init.pp (double quotes vs single quotes)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
-class mcollective( 
-  $psk            = "unset",
-  $stomp_user     = "guest",
-  $stomp_password = "guest",
-  $stomp_host     = "puppetmaster",
-  $stomp_port     = "6163",
-  $factsource     = "facter"
+class mcollective(
+  $psk            = 'unset',
+  $stomp_user     = 'guest',
+  $stomp_password = 'guest',
+  $stomp_host     = 'puppetmaster',
+  $stomp_port     = '6163',
+  $factsource     = 'facter'
 ){
   include mcollective::service
 }
