3dbd0f07bac17ee6bd12505bcf03403caa13906d
Jan Vansteenkiste <jan@vstone.eu>
2012-03-05 14:18:34 +0100

    Added default to case and fix 80char limit warning for files in plugins.pp

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -3,6 +3,7 @@ class mcollective::plugins {
   case $::operatingsystem {
     debian,ubuntu: { $p_base = '/usr/share/mcollective/plugins/mcollective' }
     redhat,centos: { $p_base = '/usr/libexec/mcollective/mcollective' }
+    default: { fail("${::hostname}: mcollective: trying to install unsupported operatingsystem ${::operatingsystem}") }
   }
 
   $s_base = 'puppet:///modules/mcollective/plugins'
@@ -15,12 +16,28 @@ class mcollective::plugins {
     notify  => Service['mcollective'],
   }
 
-  file { "${p_base}/facts/facter.rb": source => "${s_base}/facts/facter/facter.rb" }
-  file { "${p_base}/agent/service.rb": source => "${s_base}/agent/service/puppet-service.rb" }
-  file { "${p_base}/agent/package.rb": source => "${s_base}/agent/package/puppet-package.rb" }
-  file { "${p_base}/agent/nrpe.rb": source => "${s_base}/agent/nrpe/nrpe.rb" }
-  file { "${p_base}/agent/puppetd.rb": source => "${s_base}/agent/puppetd/puppetd.rb" }
-  file { "${p_base}/agent/filemgr.rb": source => "${s_base}/agent/filemgr/filemgr.rb" }
-  file { "/usr/lib/ruby/site_ruby/1.8/facter/mysql.rb": source => "${s_base}/facts/facter/mysql.rb" }
-  file { "${p_base}/facts/facter_facts.rb": source => "${s_base}/facts/facter/facter_facts.rb" }
+  file { "${p_base}/facts/facter.rb":
+    source => "${s_base}/facts/facter/facter.rb",
+  }
+  file { "${p_base}/agent/service.rb":
+    source => "${s_base}/agent/service/puppet-service.rb",
+  }
+  file { "${p_base}/agent/package.rb":
+    source => "${s_base}/agent/package/puppet-package.rb",
+  }
+  file { "${p_base}/agent/nrpe.rb":
+    source => "${s_base}/agent/nrpe/nrpe.rb",
+  }
+  file { "${p_base}/agent/puppetd.rb":
+    source => "${s_base}/agent/puppetd/puppetd.rb",
+  }
+  file { "${p_base}/agent/filemgr.rb":
+    source => "${s_base}/agent/filemgr/filemgr.rb",
+  }
+  file { '/usr/lib/ruby/site_ruby/1.8/facter/mysql.rb':
+    source => "${s_base}/facts/facter/mysql.rb",
+  }
+  file { "${p_base}/facts/facter_facts.rb":
+    source => "${s_base}/facts/facter/facter_facts.rb",
+  }
 }
