4e40056029f78ab61f4842b48c3918b1a9e871f1
Jan Vansteenkiste <jan@vstone.eu>
2012-03-05 13:04:50 +0000

    Fix variable scope for service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,6 @@
 class mcollective::service {
   case $::operatingsystem {
     ubuntu,debian,redhat,centos: { include mcollective::service::actual }
-    default: { notice("${hostname}: mcollective: module does not yet support $operatingsystem") }
+    default: { notice("${::hostname}: mcollective: module does not yet support ${::operatingsystem}") }
   }
 }
