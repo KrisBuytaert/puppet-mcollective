920ba0b8cca6d5c3de42965925158112e6bea8af
Jan Vansteenkiste <jan@vstone.eu>
2012-05-07 08:25:35 +0200

    Fix all waryrnings that are fixable

--- files/plugins/agent/nrpe/puppet/sample.pp
+++ files/plugins/agent/nrpe/puppet/sample.pp
@@ -1,5 +1,4 @@
-
-rpe_command{'check_mysqldump':
+nrpe_command{'check_mysqldump':
   command    => 'check_file_age',
   parameters => '-f /srv/backup/mysql/mysql.dump -w 86400 -c 90000 -W 8000 -C 8000'
 }
