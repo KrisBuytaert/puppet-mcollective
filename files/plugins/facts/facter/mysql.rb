17d4a65e87f573e9fad024eb5ca2fc93f2948a32
Jan Vansteenkiste <jan@vstone.eu>
2012-11-09 10:51:37 +0100

    Fixed sh: mysql not found warning that keeeeeeps repeating itself

--- files/plugins/facts/facter/mysql.rb
+++ files/plugins/facts/facter/mysql.rb
@@ -10,50 +10,56 @@
 
 # this file should be installed in your facter directory, for example on a fedora 14 it is
 # in /usr/lib/ruby/site_ruby/1.8/facter
- 
+
 # mysql command line to execute queries
-mysqlcmd = 'mysql -B -N -e'
-#status = %x[#{mysqlcmd} "SHOW STATUS"].to_s.strip
-status = %x[#{mysqlcmd} "SHOW STATUS"].split("\n")
-replica = %x[#{mysqlcmd} "SHOW SLAVE STATUS\\G"].split("\n")
+#
+
+mysql_path = '/usr/bin/mysql'
+
+if File.exists?(mysql_path)
+
+  mysqlcmd = '#{mysqlcmd} -B -N -e'
+  #status = %x[#{mysqlcmd} "SHOW STATUS"].to_s.strip
+  status = %x[#{mysqlcmd} "SHOW STATUS"].split("\n")
+  replica = %x[#{mysqlcmd} "SHOW SLAVE STATUS\\G"].split("\n")
 
 
-Facter.add(:mysql_version) do
-  setcode do
-    isinstalled = false
-    os = Facter.value('operatingsystem')
-    case os
+  Facter.add(:mysql_version) do
+    setcode do
+      isinstalled = false
+      os = Facter.value('operatingsystem')
+      case os
       when "RedHat", "CentOS", "SuSE", "Fedora"
         isinstalled = system "rpm -q mysql-server >/dev/null 2>&1"
         if not isinstalled then
-            isinstalled = system "rpm -qa Percona-Server-server >/dev/null 2>&1"
+          isinstalled = system "rpm -qa Percona-Server-server >/dev/null 2>&1"
         end
       when "Debian", "Ubuntu"
         isinstalled = system "dpkg -l mysql-server 2>&1 | egrep '(^ii|^hi)' >/dev/null"
         if not isinstalled then
-            isinstalled = system "dpkg -l Percona-server 2>&1 | egrep '(^ii|^hi)' >/dev/null"
+          isinstalled = system "dpkg -l Percona-server 2>&1 | egrep '(^ii|^hi)' >/dev/null"
         end
       else
-    end
-    if isinstalled then
-      %x[#{mysqlcmd} "SELECT VERSION()"].to_s.strip
+      end
+      if isinstalled then
+        %x[#{mysqlcmd} "SELECT VERSION()"].to_s.strip
+      end
     end
   end
-end
 
-mysqlversion = Facter.value('mysql_version')
+  mysqlversion = Facter.value('mysql_version')
 
-if mysqlversion then
+  if mysqlversion then
     Facter.add(:mysql_version_server) do
-        setcode do
-            %x[#{mysqlcmd} "SELECT @@VERSION_COMMENT"].to_s.strip
-        end
+      setcode do
+        %x[#{mysqlcmd} "SELECT @@VERSION_COMMENT"].to_s.strip
+      end
     end
     status.each do|n|
       el=n.split("\t")
       Facter.add("mysql_#{el[0]}") do
         setcode do
-            el[1]
+          el[1]
         end
       end
     end
@@ -61,8 +67,9 @@ if mysqlversion then
       el=n.split(":")
       Facter.add("mysql_replica_#{el[0].to_s.strip}") do
         setcode do
-            el[1]
+          el[1]
         end
       end
     end
+  end
 end
