0315fe23ffe58d3a3ed35ea2db8c6b44040f8eb5
Kris Buytaert <sdog@inuits.be>
2013-01-07 21:10:10 +0100

    Enabling collectives

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,12 @@
 class mcollective(
-  $psk            = 'unset',
-  $stomp_user     = 'guest',
-  $stomp_password = 'guest',
-  $stomp_host     = 'puppetmaster',
-  $stomp_port     = '61613',
-  $factsource     = 'facter'
+  $psk             = unset,
+  $stomp_user      = 'guest',
+  $stomp_password  = 'guest',
+  $stomp_host      = 'puppetmaster',
+  $stomp_port      = '61613',
+  $factsource      = 'facter',
+  $collectives     = unset,
+  $main_collective = unset,
 ){
   include mcollective::service
 }
