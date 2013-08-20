e64ca0a85efad9ff3c7b9ad7d1c73c4db59d24e4
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-08-20 10:29:38 +0200

    Add discovery plugin for puppetdb
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

---
+++ manifests/plugins/puppetdb.pp
@@ -0,0 +1,41 @@
+class mcollective::plugins::puppetdb (
+  $provider = 'package',
+) {
+
+  $packages = [ 'mcollective-puppetdb-discovery', ]
+
+  $p_base = $mcollective::plugins::p_base
+  $s_base = $mcollective::plugins::s_base
+
+  case $provider {
+
+    package : {
+      package { $packages:
+        ensure  => 'latest',
+        require => Package['mcollective'],
+        notify  => Service['mcollective'],
+      }
+    }
+
+    file : {
+      file { "${p_base}/discovery/puppetdb.rb":
+        source => "${s_base}/discovery/puppetdb/discovery/puppetdb.rb",
+        notify => Service['mcollective'],
+      }
+      file { "${p_base}/discovery/puppetd.ddl":
+        source => "${s_base}/discovery/puppetdb/discovery/puppetdb.ddl",
+        notify => Service['mcollective'],
+      }
+      file { "${p_base}/util/puppetdb_discovery.rb":
+        source => "${s_base}/discovery/puppetdb/util/puppetdb_discovery.rb",
+        notify => Service['mcollective'],
+      }
+    }
+
+    default : {
+      fail("${::hostname}: mcollective: package param only allows package or file")
+    }
+
+  }
+
+}
